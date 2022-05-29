9fd6d6d479eae458a1a9664e5a01730a020718f1
DanielMarkline <592075@sga.pucminas.br>
2022-05-29 17:12:46 -0300

    Implantação Calculadora
    Implantação Calculadora de Locações, com personalização de numero de diárias, categoria de veículos e Valor inicial da diária.

---
+++ src/teste/scripts.js
@@ -0,0 +1,7 @@
+function calculateTip(event) {
+}
+
+document.getElementById('totalTip').style.display = "none";
+document.getElementById('each').style.display = "none";
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
