b390a347f0bab8c3d8ac131239976fd90f220047
DanielMarkline <592075@sga.pucminas.br>
2022-06-21 19:55:50 -0300

    Limpeza Diretório
    Dia de faxina.

--- src/teste/scripts.js
+++
@@ -1,7 +0,0 @@
-function calculateTip(event) {
-}
-
-document.getElementById('totalTip').style.display = "none";
-document.getElementById('each').style.display = "none";
-
-document.getElementById('tipsForm').addEventListener('submit', calculateTip);
